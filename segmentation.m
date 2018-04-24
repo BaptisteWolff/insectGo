d5046798664e153c41d77b17c480a87dd4c0d0be
BaptisteWolff <baptiste.wolff@telecom-st-etienne.fr>
2018-04-24 18:17:03 +0200

    segmentation

--- segmentation.m
+++ segmentation.m
@@ -1,18 +1,38 @@
 clear all; close all; clc;
 %%
-I = imread('Images/noct02.jpg');
+% I = imread('Images/noct02.jpg');
+% I = imread('Images/_471_.jpg');
+I = imread('Images/09_09_12_170.jpg');
+% I = imread('Images/aab_0892.jpg');
+% I = imread('Images/aeshna_affinis_0x.jpg');
+% I = imread('Images/bourdonC.jpg');
+% I = imread('Images/eurytomidae_1.2_.jpg');
+% I = imread('Images/img_0597.jpg');
+% I = imread('Images/img_7252.jpg.jpg');
 % I = imread('Images/Scarabaeus-v.jpg');
 % I = imread('Images/image_galleryzoom.jpg');
 I = imresize(I,[300 300]);
-figure(1); imshow(I);
+
 
 % Segmentation 7 étapes
 Igray = rgb2gray(I);
+figure(1); imshow(Igray);
+s = strel('disk', 10);
+Igray = imopen(Igray, s);
+
+
 %%
-bw = edge(Igray, 'Canny');
+% bw = edge(Igray, 'Canny');
+% bw = edge(Igray, 'Sobel');
+bw = edge(Igray, 'Prewitt');
+% bw = edge(Igray, 'Roberts');
+% bw = edge(Igray, 'approxcanny');
+% bw = edge(Igray, 'log');
+
+% bw = bw1  bw2;
 figure(2); imshow(bw);
 %%
-s = strel('disk', 2);
+s = strel('disk', 10);
 Ic = imclose(bw, s);
 
 
