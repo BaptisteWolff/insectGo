54f0ecaa53a16c33f4824bd7338e16b5664373d1
BaptisteWolff <baptiste.wolff@telecom-st-etienne.fr>
2018-04-24 16:57:56 +0200

    Début segmentation

---
+++ segmentation.m
@@ -0,0 +1,21 @@
+clear all; close all; clc;
+%%
+I = imread('Images/noct02.jpg');
+figure(1); imshow(I);
+
+% Segmentation 7 étapes
+Igray = rgb2gray(I);
+%%
+bw = edge(Igray, 'Canny');
+figure(2); imshow(bw);
+%%
+s = strel('disk', 1);
+Ic = imclose(bw, s);
+
+figure(3); imshow(Ic);
+
+%%
+% If = imfill(Ic,'holes');
+% figure(4); imshow(If);
+
+figure(4); contour(Ic);
